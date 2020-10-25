b3ee0e4b6835f542591a5d0c25f471bcb764fc7b
Matthew Pirocchi <1623344+mpiroc@users.noreply.github.com>
2020-10-25 15:56:53 -0400

    Underline current route only.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom'
 import logo from '../logo.png';
 import './NavigationBar.css';
 
 export default function NavigationBar() {
+    const location = useLocation()
+
+    function getClassName(route) {
+        if (route === location.pathname) {
+            return 'navitem current'
+        }
+
+        return 'navitem'
+    }
+
     return (
-        <div class='navcontainer'>
-            <div className='logocontainer'>
+        <div class='nav-container'>
+            <div className='logo-container'>
                 <img className='logo' src={logo} alt='no.inc logo' />
             </div>
             
             <div className='spacer'>.</div>
             <nav>
-                <div className='navitem'><span>Home</span></div>
-                <div className='navitem'><span>Navigation 2</span></div>
-                <div className='navitem'><span>Navigation 3</span></div>
-                <div className='usernamecontainer'>
+                <div className={getClassName('/')}><span>Home</span></div>
+                <div className={getClassName('/interests')}><span>Navigation 2</span></div>
+                <div className={getClassName('/skills')}><span>Navigation 3</span></div>
+                <div>
                     <span id='username'><span id='username-icon'></span> Username</span>
                 </div>
             </nav>
