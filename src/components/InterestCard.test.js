2b4d1239cfcb477ef12714bfeb924511eca82660
Matthew Pirocchi <1623344+mpiroc@users.noreply.github.com>
2020-10-26 16:33:35 -0400

    Clean up existing tests.

--- src/components/InterestCard.test.js
+++ src/components/InterestCard.test.js
@@ -5,11 +5,12 @@ import InterestCard from './InterestCard';
 import { addInterest } from '../action-creators'
 import { createEmptyStore } from '../test-helpers'
 import 'mutationobserver-shim';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const store = createEmptyStore()
   store.dispatch(addInterest(0, 'myName', 'myType', true, 'myDetail'))
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><InterestCard id={0} /></Provider>, div);
+  ReactDOM.render(<Provider store={store}><BrowserRouter><InterestCard id={0} /></BrowserRouter></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
